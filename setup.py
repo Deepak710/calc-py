b65db248653ffa8c052f0282dbfa252f2877950f
Deepak Balaji <deepakb.messi@gmail.com>
2018-07-06 21:53:12 +0530

    setup.py
    Changed the URL from pypa to my own repo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description='A Simple Calculator Program to showcase basic python operations',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/example-project",
+    url="https://github.com/Deepak710/calc-py",
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
